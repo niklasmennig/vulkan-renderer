1b30480ae3f04ac53ca9f84b3a5c873880897241
niklasmennig <niloe99@outlook.de>
2024-01-29 13:38:31 +0100

    sync

--- src/pipeline/processing/pipeline_stage_oidn.cpp
+++ src/pipeline/processing/pipeline_stage_oidn.cpp
@@ -53,17 +53,17 @@ void ProcessingPipelineStageOIDN::on_resize(VkExtent2D swapchain_extent, VkExten
     oidn_buffer_albedo = oidnNewSharedBufferFromWin32Handle(oidn_device, OIDN_EXTERNAL_MEMORY_TYPE_FLAG_OPAQUE_WIN32, albedo_handle, nullptr, albedo_buffer->buffer_size);
     oidn_buffer_normal = oidnNewSharedBufferFromWin32Handle(oidn_device, OIDN_EXTERNAL_MEMORY_TYPE_FLAG_OPAQUE_WIN32, normals_handle, nullptr, normals_buffer->buffer_size);
 
-    oidn_filter = oidnNewFilter(oidn_device, "RT");
-    oidnSetFilterImage(oidn_filter, "color", oidn_buffer, OIDN_FORMAT_FLOAT3, render_extent.width, render_extent.height, 0, sizeof(float) * 4, 0);
-    oidnSetFilterImage(oidn_filter, "albedo", oidn_buffer_albedo, OIDN_FORMAT_FLOAT3, render_extent.width, render_extent.height, 0, sizeof(float) * 4, 0);
-    oidnSetFilterImage(oidn_filter, "normals", oidn_buffer_normal, OIDN_FORMAT_FLOAT3, render_extent.width, render_extent.height, 0, sizeof(float) * 4, 0);
-    oidnSetFilterImage(oidn_filter, "output", oidn_buffer_output, OIDN_FORMAT_FLOAT3, render_extent.width, render_extent.height, 0, sizeof(float) * 4, 0);
-    oidnCommitFilter(oidn_filter);
+    // oidn_filter = oidnNewFilter(oidn_device, "RT");
+    // oidnSetFilterImage(oidn_filter, "color", oidn_buffer, OIDN_FORMAT_FLOAT3, render_extent.width, render_extent.height, 0, sizeof(float) * 4, 0);
+    // oidnSetFilterImage(oidn_filter, "albedo", oidn_buffer_albedo, OIDN_FORMAT_FLOAT3, render_extent.width, render_extent.height, 0, sizeof(float) * 4, 0);
+    // oidnSetFilterImage(oidn_filter, "normals", oidn_buffer_normal, OIDN_FORMAT_FLOAT3, render_extent.width, render_extent.height, 0, sizeof(float) * 4, 0);
+    // oidnSetFilterImage(oidn_filter, "output", oidn_buffer_output, OIDN_FORMAT_FLOAT3, render_extent.width, render_extent.height, 0, sizeof(float) * 4, 0);
+    // oidnCommitFilter(oidn_filter);
 
-    const char* error;
-    if (oidnGetDeviceError(oidn_device, &error) != OIDN_ERROR_NONE) {
-        std::cout << "OIDN Error: " << error << std::endl;
-    }
+    // const char* error;
+    // if (oidnGetDeviceError(oidn_device, &error) != OIDN_ERROR_NONE) {
+    //     std::cout << "OIDN Error: " << error << std::endl;
+    // }
 }
 
 void ProcessingPipelineStageOIDN::process(VkCommandBuffer command_buffer, VkExtent2D swapchain_extent, VkExtent2D render_extent) {
